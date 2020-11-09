a9f100b899077d9b84bb777791b06c898b415f18
eparisot <eparisot@student.42.fr>
2020-11-09 12:40:34 +0100

    fix double SYN

--- srcs/ft_nmap.c
+++ srcs/ft_nmap.c
@@ -21,73 +21,73 @@ static void     my_packet_handler(uint8_t *args, const struct pcap_pkthdr *heade
 	char				str[str_len];
 	int 				buf_len = 1024;
 	char				*buf = (char*)malloc(buf_len);	
-    
+
 	const struct ip* iphdr;
-    const struct tcphdr* tcphdr;
+	const struct tcphdr* tcphdr;
 	const struct icmp* icmphdr;
-    const struct udphdr* udphdr;
+	const struct udphdr* udphdr;
 	char iphi[256], srcip[256], dstip[256];
 	(void)header;
 
 	ft_bzero(buf, buf_len);
 	packet += 14;
 	iphdr = (struct ip*)packet;
-    ft_strcpy(srcip, inet_ntoa(iphdr->ip_src));
-    ft_strcpy(dstip, inet_ntoa(iphdr->ip_dst));
+	ft_strcpy(srcip, inet_ntoa(iphdr->ip_src));
+	ft_strcpy(dstip, inet_ntoa(iphdr->ip_dst));
 	sprintf(iphi, "ID:%d TOS:0x%x, TTL:%d IpLen:%d DgLen:%d",
-            ntohs(iphdr->ip_id), iphdr->ip_tos, iphdr->ip_ttl,
-            4*iphdr->ip_hl, ntohs(iphdr->ip_len));
+			ntohs(iphdr->ip_id), iphdr->ip_tos, iphdr->ip_ttl,
+			4*iphdr->ip_hl, ntohs(iphdr->ip_len));
 	packet += 4*iphdr->ip_hl;
 	switch (iphdr->ip_p)
-    {
-    	case IPPROTO_TCP:
-    	    tcphdr = (struct tcphdr*)packet;
+	{
+		case IPPROTO_TCP:
+			tcphdr = (struct tcphdr*)packet;
 			ft_bzero(str, str_len);
-    	    sprintf(str, "TCP  %s:%d -> %s:%d\n", srcip, ntohs(tcphdr->th_sport),
-    	           dstip, ntohs(tcphdr->th_dport));
-    	    ft_strcat(buf, str);
+			sprintf(str, "TCP  %s:%d -> %s:%d\n", srcip, ntohs(tcphdr->th_sport),
+					dstip, ntohs(tcphdr->th_dport));
+			ft_strcat(buf, str);
 			ft_bzero(str, str_len);
 			sprintf(str, "%s\n", iphi);
 			ft_strcat(buf, str);
 			ft_bzero(str, str_len);
-    	    sprintf(str, "%c%c%c%c%c%c Seq: 0x%x Ack: 0x%x Win: 0x%x TcpLen: %d\n",
-    	           (tcphdr->th_flags & TH_URG ? 'U' : '*'),
-    	           (tcphdr->th_flags & TH_ACK ? 'A' : '*'),
-    	           (tcphdr->th_flags & TH_PUSH ? 'P' : '*'),
-    	           (tcphdr->th_flags & TH_RST ? 'R' : '*'),
-    	           (tcphdr->th_flags & TH_SYN ? 'S' : '*'),
-    	           (tcphdr->th_flags & TH_SYN ? 'F' : '*'),
-    	           ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
-    	           ntohs(tcphdr->th_win), 4*tcphdr->th_off);
+			sprintf(str, "%c%c%c%c%c%c Seq: 0x%x Ack: 0x%x Win: 0x%x TcpLen: %d\n",
+					(tcphdr->th_flags & TH_URG ? 'U' : '*'),
+					(tcphdr->th_flags & TH_ACK ? 'A' : '*'),
+					(tcphdr->th_flags & TH_PUSH ? 'P' : '*'),
+					(tcphdr->th_flags & TH_RST ? 'R' : '*'),
+					(tcphdr->th_flags & TH_SYN ? 'S' : '*'),
+					(tcphdr->th_flags & TH_FIN ? 'F' : '*'),
+					ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
+					ntohs(tcphdr->th_win), 4*tcphdr->th_off);
 			ft_strcat(buf, str);
-    	    break;
-	
-    	case IPPROTO_UDP:
-    	    udphdr = (struct udphdr*)packet;
+			break;
+
+		case IPPROTO_UDP:
+			udphdr = (struct udphdr*)packet;
 			ft_bzero(str, str_len);
-    	    sprintf(str, "UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
-    	           dstip, ntohs(udphdr->uh_dport));
+			sprintf(str, "UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
+					dstip, ntohs(udphdr->uh_dport));
 				ft_strcat(buf, str);
 			ft_bzero(str, str_len);
-    	    sprintf(str, "%s\n", iphi);
+			sprintf(str, "%s\n", iphi);
 			ft_strcat(buf, str);
-    	    break;
-	
-    	case IPPROTO_ICMP:
-    	    icmphdr = (struct icmp*)packet;
+			break;
+
+		case IPPROTO_ICMP:
+			icmphdr = (struct icmp*)packet;
 			ft_bzero(str, str_len);
-    	    sprintf(str, "ICMP %s -> %s\n", srcip, dstip);
-			ft_strcat(buf, str);
-			ft_bzero(str, str_len);
-    	    sprintf(str, "%s\n", iphi);
+			sprintf(str, "ICMP %s -> %s\n", srcip, dstip);
 			ft_strcat(buf, str);
 			ft_bzero(str, str_len);
-    	    sprintf(str, "Type:%d Code:%d ID:%d Seq:%d\n", (int)(icmphdr->icmp_type), (int)(icmphdr->icmp_code),
-    	           (int)ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), (int)ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
+			sprintf(str, "%s\n", iphi);
 			ft_strcat(buf, str);
-    	    break;
-    }
-    ft_strcat(buf, "------------------------\n");
+			ft_bzero(str, str_len);
+			sprintf(str, "Type:%d Code:%d ID:%d Seq:%d\n", (int)(icmphdr->icmp_type), (int)(icmphdr->icmp_code),
+					(int)ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), (int)ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
+			ft_strcat(buf, str);
+			break;
+	}
+	ft_strcat(buf, "------------------------\n");
 	pthread_mutex_lock(((t_probe_arg*)args)->lock);
 	if (logfile)
 		fwrite(buf, ft_strlen(buf), 1, logfile);
